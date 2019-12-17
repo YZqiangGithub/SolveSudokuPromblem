e128e38a1d50c4adb29f35c6bb3a5fe1a413b42b
YZqiangGithub <1244313829@qq.com>
2019-12-17 19:49:07 +0800

    sudokuGenerate and unit test abot it

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -4,13 +4,36 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
+#include <cstring>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#include <time.h>
+#include <fstream>
+#include <istream>
 
 using namespace std;
 
+//(7 + 3)%9 + 1 = 2 ,初始模板左上第一个数字为2
+string raw_template[9] = {
+    "234567891",
+    "891234567",
+    "567891234",
+    "123456789",
+    "456789123",
+    "789123456",
+    "912345678",
+    "345678912",
+    "678912345"
+};
+
 void SudokuSolved(char* path);
-void SudokuGenerate(long Num);
+void SudokuGenerate(int Num);
+
 
 int main(int argc, char* argv[]) {
+    /*
     if (argc != 3) {
         cout << "there are some worng in cmd params" << endl;
         exit(1);
@@ -19,7 +42,7 @@ int main(int argc, char* argv[]) {
         if (strcmp(argv[1], "-c") == 0) {
             long num = 0;
             int len = strlen(argv[2]);
-            for (int i = 0; i < len; i++) { 
+            for (int i = 0; i < len; i++) {
                 if ((argv[2][i] <= '9' && argv[2][i] >= '0') || (argv[2][i] == '-' && i == 0)) {
                     if (argv[2][i] <= '9' && argv[2][i] >= '0') {
                         num *= 10;
@@ -37,9 +60,9 @@ int main(int argc, char* argv[]) {
             if (num > 0 && num <= 1e6) {
                 SudokuGenerate(num);
             }
-			else {
-				cout << "error in number of solution, num should bigger than 0 and smaller than 1e6" << endl;
-			}
+            else {
+                cout << "error in number of solution, num should bigger than 0 and smaller than 1e6" << endl;
+            }
         }
         else if (!strcmp(argv[1], "-s")) {
             SudokuSolved(argv[2]);
@@ -49,17 +72,67 @@ int main(int argc, char* argv[]) {
             exit(1);
         }
     }
+    */
 
+    clock_t start, finish;
+    int num;
+    cin >> num;
+    start = clock();
+    remove("sudoku.txt");
+    SudokuGenerate(num);
+    finish = clock();
+    cout << "time = " << double(finish - start) / CLOCKS_PER_SEC << "s" << endl;
     return 0;
 }
 
 void SudokuSolved(char* path) {
-    
+
     return;
 }
 
-void SudokuGenerate(long Num) {
-    
+void SudokuGenerate(int Num) {
+    ofstream context;
+    context.open("sudoku.txt", ios::app);
+    int array[] = { 2,1,3,4,5,6,7,8,9 };
+    int order[] = { 0,1,2,3,4,5,6,7,8 };
+    int trans[9];
+    int new_row[9][9];
+    int temp_point = 0;
+
+    do {
+        for (int i = 0; i < 9; i++) {
+            trans[raw_template[0][i] - 49] = array[i];
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                new_row[i][j] = trans[raw_template[i][j] - 49];
+            }
+        }
+        for (int i = 0; i < 2 && Num; i++) {
+            for (int j = 0; j < 6 && Num; j++) {
+                for (int k = 0; k < 6 && Num; k++) {
+                    for (int l = 0; l < 9; l++) {
+                        for (int m = 0; m < 8; m++) {
+                            context << new_row[order[l]][m] << ' ';
+                        }
+                        context << new_row[order[l]][8];
+                        if (l != 8) context << '\n';
+                    }
+                    if (--Num) {
+                        context << '\n' << '\n';
+                    }
+                    else {
+                        context.close();
+                        return;
+                    }
+                    next_permutation(order + 6, order + 9);
+                }
+                next_permutation(order + 3, order + 6);
+            }
+            next_permutation(order + 1, order + 3);
+        }
+    } while (next_permutation(array + 1, array + 9));
+    context.close();
     return;
 }
 
---
+++ UnitTest/UnitTestGenerate.cpp
@@ -0,0 +1,163 @@
+﻿// UnitTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <time.h>
+#include <cstring>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int matrix[9][9];
+    int total = 1;
+    string path;
+    cout << "input the test file path: ";
+    getline(cin, path);
+    ifstream File(path);
+    if (!File.is_open()) {
+        cout << "error in path inputed" << endl;
+        exit(1);
+    }
+    while (!File.eof()) {
+        int is_error = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                File >> matrix[i][j];
+            }
+        }
+        for (int i = 0; i < 9; i++) {
+        int whether_repeat[9] = { 0 };
+            for (int j = 0; j < 9; j++) {
+                if (!whether_repeat[matrix[i][j] - 1]) {
+                    whether_repeat[matrix[i][j] - 1 ] = 1;
+                }
+                else {
+                    cout << "ERROR in row" << endl;
+                    is_error = 1;
+                    break;
+                }
+            }
+            if (is_error) {
+                break;
+            }
+        }
+        if (is_error) continue;
+        
+        for (int j = 0; j < 9; j++) {
+            int whether_repeat[9] = { 0 };
+            for (int i = 0; i < 9; i++) {
+                if (!whether_repeat[matrix[i][j] - 1]) {
+                    whether_repeat[matrix[i][j] - 1] = 1;
+                }
+                else {
+                    cout << "ERROE in column" << endl;
+                    is_error = 1;
+                    break;
+                }
+            }
+            if (is_error) {
+                break;
+            }
+        }
+        if (is_error) continue;
+
+        for (int i = 1; i < 8; i += 3) {
+            for (int j = 1; j < 8; j += 3) {
+                int whether_repeat[9] = { 0 };
+                if (!whether_repeat[matrix[i - 1][j - 1] - 1]) {
+                    whether_repeat[matrix[i - 1][j - 1] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i - 1][j] - 1]) {
+                    whether_repeat[matrix[i - 1][j] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i - 1][j + 1] - 1]) {
+                    whether_repeat[matrix[i - 1][j + 1] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i][j - 1] - 1]) {
+                    whether_repeat[matrix[i][j - 1] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i][j] - 1]) {
+                    whether_repeat[matrix[i][j] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i][j + 1] - 1]) {
+                    whether_repeat[matrix[i][j + 1] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i + 1][j - 1] - 1]) {
+                    whether_repeat[matrix[i + 1][j - 1] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i + 1][j] - 1]) {
+                    whether_repeat[matrix[i + 1][j] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (!whether_repeat[matrix[i + 1][j + 1] - 1]) {
+                    whether_repeat[matrix[i + 1][j + 1] - 1] = 1;
+                }
+                else
+                {
+                    is_error = 1;
+                }
+                if (is_error) break;
+            }
+            if (is_error) break;
+        }
+
+        if (is_error) {
+            total = 0;
+            cout << "ERROR in block" << endl;
+            return 0;
+        }
+    }
+    if (!total) {
+        cout << "Wrong answer found" << endl;
+    }
+    else cout << "Correct answer" << endl;
+
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
